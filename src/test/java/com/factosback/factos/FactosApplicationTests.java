8b2896be588b723a39718808a0a9e5217a0cae94
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-02 13:48:24 +0900

    Initial project setup

---
+++ src/test/java/com/factosback/factos/FactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factosback.factos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
