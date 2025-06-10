3ee592ac5ca106b0155ed553aa90392a5007f0ed
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-10 21:54:59 +0900

    [Ci/#7 ] 서브모듈 추가 (#8)
    * Ci: submodule 추가
    
    * Docs: config file 수정
    
    * Docs: config 수정

---
+++ src/main/java/com/factosback/factos/global/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.factosback.factos.global.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig {
+}
