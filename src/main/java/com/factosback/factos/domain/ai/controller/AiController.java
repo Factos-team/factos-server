c197c5472fc2f6e326f92d8b20b30c4cb8a81967
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-10 09:58:03 +0900

    Feat: Controller 추가
    - AiController
    - PrecedentController
    - TermController

---
+++ src/main/java/com/factosback/factos/domain/ai/controller/AiController.java
@@ -0,0 +1,12 @@
+package com.factosback.factos.domain.ai.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+//@RequestMapping("/ai")
+public class AiController {
+
+}
---
+++ src/main/java/com/factosback/factos/domain/term/controller/TermController.java
@@ -0,0 +1,11 @@
+package com.factosback.factos.domain.term.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+//@RequestMapping("/term")
+public class TermController {
+}
