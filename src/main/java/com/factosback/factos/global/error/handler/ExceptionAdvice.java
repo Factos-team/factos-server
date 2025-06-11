c7d48f6a55da9369be9759f896243e509b207f41
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-11 13:50:32 +0900

    [Feat/#13] base code 구현 (#14)
    * Feat: Error Code 추가
    
    * Feat: API 예외 발생 시 사용할 예외 클래스 추가
    
    * Feat: ExceptionAdvice 추가
    
    * Feat: ResponseEntityExceptionHandler 상속 추가
    
    * Refactor: 부모 클래스 메서드명과 동일하게 변경
    
    * Refactor: Override문 status에서 statusCode로 변경
    
    * Feat: ApiResponse 구현
    
    * Refactor: 실패응답 <?>로 변경
    
    * Feat: WebConfig 추가
    
    * Feat: Authorization 주석

---
+++ src/main/java/com/factosback/factos/global/error/handler/ExceptionAdvice.java
@@ -0,0 +1,69 @@
+package com.factosback.factos.global.error.handler;
+
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.factosback.factos.global.error.code.CommonErrorCode;
+import com.factosback.factos.global.error.code.ErrorCode;
+import com.factosback.factos.global.error.exception.RestApiException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(RestApiException.class)
+	public ResponseEntity<Object> handleRestApiException(RestApiException e) {
+		ErrorCode errorCode = e.getErrorCode();
+		return handleExceptionInternal(errorCode);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
+		log.warn("handlerIllegalArgument 입니다.");
+		ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+		return handleExceptionInternal(errorCode, e.getMessage());
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		log.warn("handleHttpRequestMethodNotSupportedException 입니다.");
+		ErrorCode errorCode = CommonErrorCode.METHOD_NOT_ALLOWED;
+		return handleExceptionInternal(errorCode);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		log.warn("handleMethodArgumentNotValidException 입니다.");
+		ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+		return handleExceptionInternal(errorCode, e.getMessage());
+	}
+
+	private static String getDefaultMessage(MethodArgumentNotValidException e) {
+		return e.getBindingResult()
+			.getAllErrors()
+			.stream()
+			.map(ObjectError::getDefaultMessage)
+			.collect(Collectors.joining("\u00a0"));
+	}
+
+	private ResponseEntity<Object> handleExceptionInternal (final ErrorCode errorCode) {
+		return ResponseEntity.status(errorCode.getHttpStatus()).body(errorCode);
+	}
+
+	private ResponseEntity<Object> handleExceptionInternal (final ErrorCode errorCode, final String message) {
+		return ResponseEntity.status(errorCode.getHttpStatus()).body(message);
+	}
+}
