bb096f932f350e24ea4278d6b54bab9b5be96319
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-12 01:16:56 +0900

    [Feat/#21] webclient (#22)
    * Build: webClient 의존성 추가
    
    * Feat: 컬럼 추가
    
    * Feat: WebClientConfig 구현
    
    * Feat: webClient 기본 베이스 코드 구현

---
+++ src/main/java/com/factosback/factos/global/config/WebClientConfig.java
@@ -0,0 +1,31 @@
+package com.factosback.factos.global.config;
+
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebClientConfig {
+
+	/**
+	 * 참고 webClient 기본 base code
+	 */
+	@Bean
+	public WebClient webClient(WebClient.Builder builder) {
+		return builder
+			.baseUrl("호출할 API 서비스 도메인 URL")
+			.defaultHeaders(httpHeaders -> {
+				httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+				httpHeaders.add("apiKey", "API Key 값 입력");
+			})
+			// 필요한 설정 추가
+			.build();
+	}
+}
