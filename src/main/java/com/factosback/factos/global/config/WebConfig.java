2b782e3dfdb257cfe30ffb304275c10112b27840
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-11 11:56:38 +0900

    Feat: WebConfig 추가

--- src/main/java/com/factosback/factos/global/config/WebConfig.java
+++ src/main/java/com/factosback/factos/global/config/WebConfig.java
@@ -1,10 +1,21 @@
 package com.factosback.factos.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		corsRegistry.addMapping("/**")
+			.allowedOrigins("*")
+			.exposedHeaders("Authorization")
+			.allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+			.maxAge(3600);
+	}
 }
