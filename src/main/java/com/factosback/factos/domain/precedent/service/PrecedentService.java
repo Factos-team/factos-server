7f4be257a2469af848f2320b79d0d0915124a2f4
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-11 18:31:50 +0900

    [Feat/#15] 사건번호 검색 API 구현 (#16)
    * Feat: Precedent DTO 추가
    
    * Feat: Precedent Converter 추가
    
    * Feat: Precedent 엔티티 컬럼명 수정
    
    * Feat: Precedent Error code 추가
    
    * Feat: findByCaseNumber 추가
    
    * Feat: Precedent Controller 추가
    
    * Feat: Precedent Service 추가
    
    * Refactor: created_at 수정 못하도록 변경
    
    * Refactor: 엔티티 관계 변경에 따른 컬럼 수정
    
    * Feat: 추후 검색저장 기능 도입을 위해 findByCaseNumber 추가

--- src/main/java/com/factosback/factos/domain/precedent/service/PrecedentService.java
+++ src/main/java/com/factosback/factos/domain/precedent/service/PrecedentService.java
@@ -3,8 +3,13 @@ package com.factosback.factos.domain.precedent.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.factosback.factos.domain.precedent.converter.PrecedentConverter;
+import com.factosback.factos.domain.precedent.dto.GetPrecedentDto;
+import com.factosback.factos.domain.precedent.error.PrecedentErrorCode;
+import com.factosback.factos.domain.precedent.model.Precedent;
 import com.factosback.factos.domain.precedent.repository.PrecedentRepository;
 import com.factosback.factos.domain.precedent.repository.PrecedentSearchRepository;
+import com.factosback.factos.global.error.exception.RestApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,4 +20,12 @@ public class PrecedentService {
 
 	private final PrecedentRepository precedentRepository;
 	private final PrecedentSearchRepository precedentSearchRepository;
+
+	public GetPrecedentDto.Response getPrecedent(Integer caseNumber) {
+
+		Precedent precedent = precedentRepository.findByCaseNumber(caseNumber)
+			.orElseThrow(() -> new RestApiException(PrecedentErrorCode.PRECEDENT_NOT_FOUND));
+
+		return PrecedentConverter.convertToGetPrecedentDto(precedent);
+	}
 }
