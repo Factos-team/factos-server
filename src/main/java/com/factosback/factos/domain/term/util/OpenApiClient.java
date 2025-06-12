963700129ce0a76fe4fde7f6a911198aba729900
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-12 22:48:44 +0900

    Feat: open API 응답 오류 추적 로그 추가

--- src/main/java/com/factosback/factos/domain/term/util/OpenApiClient.java
+++ src/main/java/com/factosback/factos/domain/term/util/OpenApiClient.java
@@ -1,14 +1,22 @@
 package com.factosback.factos.domain.term.util;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.factosback.factos.domain.term.dto.TranslateTermDto;
+import com.factosback.factos.global.error.code.CommonErrorCode;
+import com.factosback.factos.global.error.exception.RestApiException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OpenApiClient {
@@ -23,10 +31,21 @@ public class OpenApiClient {
 				.queryParam("OC", request.getOc())
 				.queryParam("target", request.getTarget())
 				.queryParam("type", request.getType())
-				.queryParam("query", request.getQuery())
+				.queryParam("query", URLEncoder.encode(request.getQuery(), StandardCharsets.UTF_8))
 				.build())
 			.retrieve()
+			.onStatus(HttpStatusCode::isError, response -> {
+				return response.bodyToMono(String.class)
+					.flatMap(body -> {
+						log.error("API 오류 응답: {}", body);
+						return Mono.error(new RestApiException(CommonErrorCode.INTERNAL_SERVER_ERROR));
+					});
+			})
 			.bodyToMono(String.class)
+			.map(response -> {
+				log.info("API Raw Response: {}", response);
+				return response;
+			})
 			.map(openApiResponseParser::extractGeneralTerms)
 			.block();
 	}
