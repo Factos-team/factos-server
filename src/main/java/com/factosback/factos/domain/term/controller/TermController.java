9f5d1596b3d28178677a6eb41e470598e9349a3c
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-13 01:42:33 +0900

    [Feat/#23] 법령 용어, 일상 용어 변환 API 구현 (#24)
    * Feat: openApiWebClient 추가
    
    * Feat: dto 추가
    
    * Feat: converter 추가
    
    * Feat: controller 추가
    
    * Feat: OpenApiClient 추가
    
    * Feat: OpenApiClient 파싱 로직 담당 클래스 추가
    
    * Feat: service 임시 구현
    
    * Feat: 컬럼명 변경 및 업데이트 코드 구현
    
    * Feat: 파싱 로직 구현 및 로그 추가
    
    * Feat: 파싱 로직 추가
    
    * Feat: 일반 용어 엔티티 추가
    
    * Feat: 관계 추가
    
    * Refactor: openApi 관련 애노테이션 수정
    
    * Refactor: openApi 관련 converter 수정
    
    * Refactor: List 형식으로 변환
    
    * Refactor: 파싱 로직 수정
    
    * Refactor: processTranslation 로직 수정
    
    * Refactor: processTranslation 로직 수정
    
    * Refactor: 애노테이션 추가
    
    * Refactor: generalTerm 주석 처리
    
    * Test: parser 동작 테스트
    
    * Feat: open API 응답 오류 추적 로그 추가
    
    * Feat: open API 응답 오류 추적 로그 추가
    
    * Feat: 오류 확인 코드 추가
    
    * Refactor: encoding 구문 제거
    
    * Refactor: json 형식에 따른 node 변경
    
    * Test: 실제 받은 json 데이터로 parser 테스트
    
    * Refactor: 실제 메일 연동 확인, @Value 도입 예정
    
    * Refactor: 대용량 응답 대비 코드 추가 및 defaultHeader 변경
    
    * Test: WebClient가 생성하는 URL 테스트
    
    * Feat: Builder.Default 어노테이션 추가
    
    * Feat: 오류 해결 후 로그 관련 코드 삭제
    
    * Docs: config 파일 수정
    
    * Feat: ConfigurationProperties 어노테이션을 이용한 설정 관리 코드 추가
    
    * Feat: ConfigurationPropertiesScan 어노테이션 추가
    
    * Refactor: 로그 삭제
    
    * Chore: 쓰지 않는 import 구문 삭제
    
    * Refactor: 설정값 외부화
    
    * Refactor: 설정값 외부화

--- src/main/java/com/factosback/factos/domain/term/controller/TermController.java
+++ src/main/java/com/factosback/factos/domain/term/controller/TermController.java
@@ -1,11 +1,29 @@
 package com.factosback.factos.domain.term.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.factosback.factos.domain.term.dto.TranslateTermDto;
+import com.factosback.factos.domain.term.service.TermService;
+import com.factosback.factos.global.response.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/term")
+@RequestMapping("/api/terms")
 public class TermController {
+
+	private final TermService termService;
+
+	@PostMapping("/translate")
+	public ApiResponse<TranslateTermDto.Response> translateTerm(@RequestBody TranslateTermDto.UserInputRequest request) {
+
+		// 현재 로그인 미구현 상태이므로 Mock 처리
+		TranslateTermDto.Response response = termService.processTranslation(request, null);
+
+		return ApiResponse.createSuccess(response);
+	}
 }
--- src/main/java/com/factosback/factos/domain/term/model/TermTranslation.java
+++ src/main/java/com/factosback/factos/domain/term/model/TermTranslation.java
@@ -1,8 +1,12 @@
 package com.factosback.factos.domain.term.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.factosback.factos.domain.member.model.Member;
 import com.factosback.factos.global.common.model.BaseEntity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -10,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,13 +32,22 @@ public class TermTranslation extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String legal_term;
+	private String legalTerm;
 
-	private String general_term;
+	// private String generalTerm;
 
 	private String content;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
+
+	@OneToMany(mappedBy = "termTranslation", cascade = CascadeType.ALL, orphanRemoval = true)
+	@Builder.Default
+	private List<GeneralTerm> generalTerms = new ArrayList<>();
+
+	public void addGeneralTerms(List<GeneralTerm> generalTerms) {
+		this.generalTerms.addAll(generalTerms);
+		generalTerms.forEach(gt -> gt.setTermTranslation(this));
+	}
 }
