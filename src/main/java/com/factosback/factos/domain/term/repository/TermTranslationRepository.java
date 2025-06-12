57dcf96d489300906c59df0eff4cf751fd142053
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-13 01:48:21 +0900

    Feat: findByLegalTerm 메서드 추가

--- src/main/java/com/factosback/factos/domain/term/repository/TermTranslationRepository.java
+++ src/main/java/com/factosback/factos/domain/term/repository/TermTranslationRepository.java
@@ -1,9 +1,11 @@
 package com.factosback.factos.domain.term.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.factosback.factos.domain.term.model.TermTranslation;
 
 public interface TermTranslationRepository extends JpaRepository<TermTranslation, Long> {
-
+	Optional<TermTranslation> findByLegalTerm(String legalTerm);
 }
