45686890b745b3e2cff9822eba091a905a196a19
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-13 03:50:53 +0900

    Refactor: List 형식으로 수정

--- src/main/java/com/factosback/factos/domain/term/repository/TermTranslationRepository.java
+++ src/main/java/com/factosback/factos/domain/term/repository/TermTranslationRepository.java
@@ -1,11 +1,11 @@
 package com.factosback.factos.domain.term.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.factosback.factos.domain.term.model.TermTranslation;
 
 public interface TermTranslationRepository extends JpaRepository<TermTranslation, Long> {
-	Optional<TermTranslation> findByLegalTerm(String legalTerm);
+	List<TermTranslation> findByLegalTerm(String legalTerm);
 }
