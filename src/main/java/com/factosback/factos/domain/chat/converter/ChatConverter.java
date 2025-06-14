da92e5f8ffa98f389bf8a2b3387f725212792d76
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-14 09:55:49 +0900

    Refactor: 수정된 dto 반영

--- src/main/java/com/factosback/factos/domain/chat/converter/ChatConverter.java
+++ src/main/java/com/factosback/factos/domain/chat/converter/ChatConverter.java
@@ -2,12 +2,14 @@ package com.factosback.factos.domain.chat.converter;
 
 import com.factosback.factos.domain.chat.dto.ChatMessageDto;
 import com.factosback.factos.domain.chat.model.ChatMessage;
-import com.factosback.factos.domain.member.model.Member;
+import com.factosback.factos.domain.chat.model.ChatRoom;
 
 public class ChatConverter {
 
-	public static ChatMessage convertToChatMessage(ChatMessageDto.UserInputRequest request, Member member) {
+	public static ChatMessage convertToChatMessage(ChatMessageDto.UserInputRequest request, ChatRoom chatRoom) {
+
 		return ChatMessage.builder()
+			.chatRoom(chatRoom)
 			.case_summary(request.getCaseSummary())
 			.member_evidence(request.getMemberEvidence())
 			.opponent_claim(request.getOpponentClaim())
