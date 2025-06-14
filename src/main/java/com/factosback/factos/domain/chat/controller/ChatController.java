040b81892fa7dd5757693502cea25f187de6cf6c
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-14 13:38:00 +0900

    [Feat/#29] AI 법률 대응 시뮬레이터 구현 (#30)
    * Feat: ChatMessageDto 추가
    
    * Feat: AiRequestDto 추가
    
    * Feat: aiWebClient 추가
    
    * Feat: record 추가
    
    * Feat: 대용량 응답 관련 코드 추가
    
    * Feat: ai api key 관련 코드 추가
    
    * Comment: ai key 관련 임시 주석
    
    * Feat: AiClient 추가
    
    * Feat: ChatService 추가
    
    * Feat: requestMapping 추가
    
    * Feat: chat 관련 메소드 converter로 변환
    
    * Feat: ai 관련 메소드 converter로 변환
    
    * Feat: 양방향 연관 관계 설정
    
    - ChatMessage
    - AiReply
    
    * Refactor: converter 사용하도록 변경
    
    * Comment: base code 주석처리
    
    * Feat: 연관관계 추가
    
    * Feat: 연관관계 추가
    
    * Refactor: UserInputRequest dto 필드 추가
    
    * Refactor: 수정된 dto 반영
    
    * Refactor: 트랜잭션 원자성 보장하기 위한 로직 순서 변경
    
    * Refactor: getAiReply 로 변경
    
    * Feat: chatErrorCode 추가
    
    * Feat: 예외 메시지 로깅 추가
    
    * Feat: 컨트롤러 추가
    
    * Refactor: pathVariable로 받도록 수정
    
    * Refactor: 필요없는 필드 삭제
    
    * Refactor: pathVariable 사용에 따른 코드 변경
    
    * Refactor: AI 서버 기능 구현 전까지 임시 데이터 반환하도록 수정
    
    * Docs: config 수정
    
    * Refactor: 엔티티 매핑 수정
    
    * Refactor: mysql에서 postgresql로 변경에 따라 default 구문 삭제
    
    * Remove: 쓰지 않는 controller test 삭제
    
    * Refactor: mock data 수정
    
    * Feat: noHandlerFoundException 예외 처리
    
    * Feat: error code 추가
    
    * Feat: handleNoHandlerFoundException 예외 처리 추가
    
    * Feat: handleNoHandlerFoundException 예외 처리 추가
    
    * Docs: config 수정
    
    * Docs: config 수정
    
    * Feat: pgvector 사용하기 위한 엔티티 추가

--- src/main/java/com/factosback/factos/domain/chat/controller/ChatController.java
+++ src/main/java/com/factosback/factos/domain/chat/controller/ChatController.java
@@ -1,13 +1,29 @@
 package com.factosback.factos.domain.chat.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.factosback.factos.domain.chat.dto.ChatMessageDto;
+import com.factosback.factos.domain.chat.service.ChatService;
+import com.factosback.factos.global.response.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/chat")
+@RequestMapping("/api/chat")
 public class ChatController {
 
+	private final ChatService chatService;
+
+	@PostMapping("/{chatRoomId}/send")
+	public ApiResponse<ChatMessageDto.AiResponse> processChatMessage(@PathVariable Long chatRoomId, @RequestBody ChatMessageDto.UserInputRequest request) {
+
+		ChatMessageDto.AiResponse response = chatService.processChatMessage(chatRoomId, request).getData();
+
+		return ApiResponse.createSuccess(response);
+	}
 }
