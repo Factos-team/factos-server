f89fd1c9b7fdbbeff1b537ae159e6494b5ece4ed
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-14 10:12:55 +0900

    Feat: 컨트롤러 추가

--- src/main/java/com/factosback/factos/domain/chat/controller/ChatController.java
+++ src/main/java/com/factosback/factos/domain/chat/controller/ChatController.java
@@ -1,13 +1,28 @@
 package com.factosback.factos.domain.chat.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.factosback.factos.domain.chat.dto.ChatMessageDto;
+import com.factosback.factos.domain.chat.service.ChatService;
+import com.factosback.factos.global.response.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/chats")
+@RequestMapping("/api/chat")
 public class ChatController {
 
+	private final ChatService chatService;
+
+	@PostMapping("/send")
+	public ApiResponse<ChatMessageDto.AiResponse> processChatMessage(@RequestBody ChatMessageDto.UserInputRequest request) {
+
+		ChatMessageDto.AiResponse response = chatService.processChatMessage(request).getData();
+
+		return ApiResponse.createSuccess(response);
+	}
 }
