c538a6817fd9e8056f5994ecbcceb5d1d1e8c78f
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-15 19:59:23 +0900

    Feat: profile 설정

--- src/main/java/com/factosback/factos/domain/ai/util/AiClient.java
+++ src/main/java/com/factosback/factos/domain/ai/util/AiClient.java
@@ -1,5 +1,6 @@
 package com.factosback.factos.domain.ai.util;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Profile("!local")
 @Slf4j
 @Component
 @RequiredArgsConstructor
