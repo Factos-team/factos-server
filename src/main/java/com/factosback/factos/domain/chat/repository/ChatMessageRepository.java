1ce9311a3c587a230db212c1df589b5bc4fff528
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-15 18:44:41 +0900

    Feat: 조회 로직 추가

--- src/main/java/com/factosback/factos/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/factosback/factos/domain/chat/repository/ChatMessageRepository.java
@@ -1,8 +1,13 @@
 package com.factosback.factos.domain.chat.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.factosback.factos.domain.chat.model.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+	Optional<ChatMessage> findLatestByChatRoomId(@Param("chatRoomId") Long chatRoomId);
 }
