032fb3af1d6ce1851244ef75f06c13829edcd6d6
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-17 16:18:32 +0900

    Refactor: 테이블 수정에 따른 엔티티 수정

--- src/main/java/com/factosback/factos/domain/precedent/model/Precedent.java
+++ src/main/java/com/factosback/factos/domain/precedent/model/Precedent.java
@@ -1,11 +1,21 @@
 package com.factosback.factos.domain.precedent.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.factosback.factos.domain.member.model.Member;
 import com.factosback.factos.global.common.model.BaseEntity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,11 +33,15 @@ public class Precedent extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String title;
+	private String caseNumber;
 
-	private Integer caseNumber;
+	@LastModifiedDate
+	private LocalDateTime searchedAt;
 
-	private String caseDescription;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
 
-	private String judgment;
+	@OneToOne(mappedBy = "precedent", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private PrecedentReply precedentReply;
 }
