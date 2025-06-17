9e08f5a4d0fa8876255700a718b002667abc4cf5
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-17 20:36:21 +0900

    Comment: 기존 기능 주석 처리

--- src/main/java/com/factosback/factos/global/util/OpenApiClient.java
+++ src/main/java/com/factosback/factos/global/util/OpenApiClient.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.factosback.factos.domain.precedent.dto.PrecedentReplyDto;
-import com.factosback.factos.domain.term.dto.TranslateTermDto;
 import com.factosback.factos.global.config.OpenApiProperties;
 import com.factosback.factos.global.error.code.CommonErrorCode;
 import com.factosback.factos.global.error.exception.RestApiException;
@@ -90,31 +89,29 @@ public class OpenApiClient {
 		return OpenApiResponseParser.extractPrecedentContent(response);
 	}
 
-	/**
-	 * 용어 변환 조회 API
-	 */
-	public List<String> getGeneralTerms(TranslateTermDto.OpenApiRequest request) {
-
-		return openApiWebClient.get()
-			.uri(uriBuilder -> uriBuilder
-				.path("/lawService.do")
-				.queryParam("OC", request.getOc())
-				.queryParam("target", request.getTarget())
-				.queryParam("type", request.getType())
-				.queryParam("query", request.getQuery())
-				.build())
-			.retrieve()
-			.onStatus(HttpStatusCode::isError, response -> {
-				return response.bodyToMono(String.class)
-					.flatMap(body -> {
-						log.error("API 오류 응답: {}", body);
-						return Mono.error(new RestApiException(CommonErrorCode.INTERNAL_SERVER_ERROR));
-					});
-			})
-			.bodyToMono(String.class)
-			.map(openApiResponseParser::extractGeneralTerms)
-			.block();
-	}
-
-
+	// /**
+	//  * 용어 변환 조회 API
+	//  */
+	// public List<String> getGeneralTerms(TranslateTermDto.OpenApiRequest request) {
+	//
+	// 	return openApiWebClient.get()
+	// 		.uri(uriBuilder -> uriBuilder
+	// 			.path("/lawService.do")
+	// 			.queryParam("OC", request.getOc())
+	// 			.queryParam("target", request.getTarget())
+	// 			.queryParam("type", request.getType())
+	// 			.queryParam("query", request.getQuery())
+	// 			.build())
+	// 		.retrieve()
+	// 		.onStatus(HttpStatusCode::isError, response -> {
+	// 			return response.bodyToMono(String.class)
+	// 				.flatMap(body -> {
+	// 					log.error("API 오류 응답: {}", body);
+	// 					return Mono.error(new RestApiException(CommonErrorCode.INTERNAL_SERVER_ERROR));
+	// 				});
+	// 		})
+	// 		.bodyToMono(String.class)
+	// 		.map(openApiResponseParser::extractGeneralTerms)
+	// 		.block();
+	// }
 }
