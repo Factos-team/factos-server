5a40e422ec1ee7201bfd80339b939809f8e27117
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-17 19:18:52 +0900

    Refactor: 영속성 컨텍스트 동기화 문제로 인해 EntityGraph 어노테이션 사용

--- src/main/java/com/factosback/factos/domain/term/repository/TermRepository.java
+++ src/main/java/com/factosback/factos/domain/term/repository/TermRepository.java
@@ -2,11 +2,14 @@ package com.factosback.factos.domain.term.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.factosback.factos.domain.term.model.Term;
 
 public interface TermRepository extends JpaRepository<Term, Long> {
 
+	// termReply를 즉시 로딩(EAGER)로 가져옴
+	@EntityGraph(attributePaths = {"termReply"})
 	Optional<Term> findByLegalTerm(String legalTerm);
 }
