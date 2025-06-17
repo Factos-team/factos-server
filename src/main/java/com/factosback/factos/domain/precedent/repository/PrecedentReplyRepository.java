24b5092a4dc5a9a2e842a6ecb8043da586419c56
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-17 16:20:15 +0900

    Feat: 판례 관련 Repository 추가

---
+++ src/main/java/com/factosback/factos/domain/precedent/repository/PrecedentReplyRepository.java
@@ -0,0 +1,10 @@
+package com.factosback.factos.domain.precedent.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.factosback.factos.domain.precedent.model.PrecedentReply;
+
+public interface PrecedentReplyRepository extends JpaRepository<PrecedentReply, Long> {
+
+
+}
