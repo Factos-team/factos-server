630e1113e2dca540c2817d756d86ff0d9fdfc0e2
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-06-17 16:19:39 +0900

    Feat: 판례 관련 응답 테이블 추가

---
+++ src/main/java/com/factosback/factos/domain/precedent/model/PrecedentReply.java
@@ -0,0 +1,35 @@
+package com.factosback.factos.domain.precedent.model;
+
+import com.factosback.factos.global.common.model.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PrecedentReply extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(columnDefinition = "TEXT")
+	private String claudeResponse;
+
+	@OneToOne
+	@JoinColumn(name = "precedent_id")
+	private Precedent precedent;
+}
